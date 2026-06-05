7bbb7b6ec968bc4bf73a9982c62b36e1a4623873
August <legendarycloudz@vk.com>
2026-06-05 23:46:02 +0600

    Delete fix_indent_tabs.py

--- fix_indent_tabs.py
+++
@@ -1,5 +0,0 @@
-with open("blackjack.py", "r") as f:
-    lines = f.readlines()
-
-# Indentation error on 824 means something right above it or below it is messed up.
-# Let's completely wipe that setup_roulette_screen and draw_roulette_table blocks and write them cleanly.
